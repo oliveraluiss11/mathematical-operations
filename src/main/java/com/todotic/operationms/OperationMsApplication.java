7e3c6bd3753ff35b1f7219c0234d0c63674d5870
Luis Olivera <loliverv11@gmail.com>
2023-11-20 08:27:08 -0500

    Proyecto estabilizado

---
+++ src/main/java/com/todotic/operationms/OperationMsApplication.java
@@ -0,0 +1,13 @@
+package com.todotic.operationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationMsApplication.class, args);
+	}
+
+}
